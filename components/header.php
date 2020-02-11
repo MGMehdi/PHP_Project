3dc7963d8352cebe6b5e8362e9e641648c85d8d6
Mehdi <mgmehdi>
2020-02-11 13:48:03 +0100

    yes

---
+++ components/header.php
@@ -0,0 +1,15 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../bootstrap.css">
+    <link rel="stylesheet" href="../style.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <img src="../images/terroir-2-1.jpg" id="banner">
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,22 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="bootstrap.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    include 'components/header.php';
+    include 'components/nav.php'
+    ?>
+    <p class="h2">COUCOU</p>
+    <?php
+    echo "LOLOLO";
+    ?>
+
+</body>
+
+</html>
