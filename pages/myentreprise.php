a5ca175721489e0840ce053838a5545a5b3759ef
Mehdi Grira <mgmehdi@tutanota.com>
2020-05-17 15:01:25 +0200

    Contact for help

--- pages/myentreprise.php
+++ pages/myentreprise.php
@@ -9,93 +9,95 @@ $action = '';
 $entreprises = $db->GetMyEntreprises($_SESSION['id']);
 
 if (isset($_POST['NewEntreprise'])) {
-    header('Location: addentreprise.php');
+	header('Location: addentreprise.php');
 }
 
 if (isset($_POST['modify'])) {
-    $_SESSION['idEntreprise'] = $_POST['idEntreprise'];
-    header('Location: updateEntreprise.php');
+	$_SESSION['idEntreprise'] = $_POST['idEntreprise'];
+	header('Location: updateEntreprise.php');
 }
 
 if (isset($_POST['Oui'])) {
-    $db->DeleteEntreprise($_POST['idEntreprise']);
-    header("Refresh:0");
+	$db->DeleteEntreprise($_POST['idEntreprise']);
+	header("Refresh:0");
 }
 ?>
 
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/bootstrap.css">
-    <link rel="stylesheet" href="../css/style.css">
-    <title>Mes entreprises</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="../css/bootstrap.css">
+	<link rel="stylesheet" href="../css/style.css">
+	<title>Mes entreprises</title>
 </head>
 
 <body>
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-sm-2 text-center">
-                <form action="myentreprise.php" method="post">
-                    <input type="submit" class="btn btn-secondary" value="Nouvelle entreprise" name="NewEntreprise">
-                </form>
-            </div>
-            <div class="col-sm-10">
-                <table class="table text-center">
-                    <thead>
-                        <th scope="col">Nom</th>
-                        <th scope="col">Adresse</th>
-                        <th scope="col">Produit</th>
-                        <th scope="col">Contact</th>
-                        <th scope="col">Action</th>
-                    </thead>
-                    <tbody>
-                        <?php foreach ($entreprises as $entreprise) { ?>
-                            <tr>
-                                <td><?php echo htmlspecialchars($entreprise->getName()) ?></td>
-                                <td><?php echo htmlspecialchars($entreprise->getAddress()) ?></td>
-                                <td><?php echo htmlspecialchars($entreprise->getProduct()) ?></td>
-                                <td><?php echo htmlspecialchars($entreprise->getPhone()) ?></td>
-                                <td>
-                                    <form action="myentreprise.php" method="post">
-                                        <button type="button" name="delete" onclick="Delete(<?php echo $entreprise->getId() ?>)"><img src="../images/delete.svg" alt="" srcset="" style="width: 2em;"></button>
-                                        <button type=" submit" name="modify" value="Modify"><img src="../images/edit.svg" style="width: 2em;"></button>
-                                        <input hidden type="number" name="idEntreprise" value="<?php echo $entreprise->getId() ?>">
-                                    </form>
-                                </td>
-                            </tr>
-                        <?php } ?>
-                    </tbody>
-                </table>
-            </div>
-        </div>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-sm-2 text-center">
+				<form action="myentreprise.php" method="post">
+					<input type="submit" class="btn btn-secondary" value="Nouvelle entreprise" name="NewEntreprise">
+				</form>
+			</div>
+			<div class="col-sm-10">
+				<table class="table text-center">
+					<thead>
+						<th scope="col">Nom</th>
+						<th scope="col">Adresse</th>
+						<th scope="col">Produit</th>
+						<th scope="col">Contact</th>
+						<th scope="col">Action</th>
+					</thead>
+					<tbody>
+						<?php foreach ($entreprises as $entreprise) { ?>
+							<tr>
+								<td><?php echo htmlspecialchars($entreprise->getName()) ?></td>
+								<td><?php echo htmlspecialchars($entreprise->getAddress()) ?></td>
+								<td><?php echo htmlspecialchars($entreprise->getProduct()) ?></td>
+								<td><?php echo htmlspecialchars($entreprise->getPhone()) ?></td>
+								<td>
+									<form action="myentreprise.php" method="post">
+										<button type="button" name="delete" onclick="Delete(<?php echo $entreprise->getId() ?>)"><img src="../images/delete.svg" alt="" srcset="" style="width: 2em;"></button>
+										<button type=" submit" name="modify" value="Modify"><img src="../images/edit.svg" style="width: 2em;"></button>
+										<input hidden type="number" name="idEntreprise" value="<?php echo $entreprise->getId() ?>">
+									</form>
+								</td>
+							</tr>
+						<?php } ?>
+					</tbody>
+				</table>
+			</div>
+		</div>
 
-        <div id="askDeleteBackground" class="text-center" onclick="Close()">
-            <div id="loginCard" class="card" style="width: 40rem;">
-                <div class="card-body">
-                    <h3 class="card-title">Supprimer ?</h3>
-                    <h5>Etes vous sûr de vouloir supprimer cette entrepreprise ?</h5>
-                    <form action="myentreprise.php" method="post">
-                        <input type="number" name="idEntreprise" hidden value="<?php echo $entreprise->getId() ?>">
-                        <input type="submit" name="Oui" class="btn btn-danger" value="Oui">
-                        <input type="button" name="Non" class="btn btn-success" value="Non" onclick="Close()">
-                    </form>
-                </div>
-            </div>
-        </div>
-        <!-- //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// -->
-    </div>
+		<div id="cardBackground" class="text-center" onclick="Close()"></div>
+		<div id="deleteCard" class="card" style="width: 40rem;">
+			<div class="card-body">
+				<h3 class="card-title">Supprimer ?</h3>
+				<h5>Etes vous sûr de vouloir supprimer cette entrepreprise ?</h5>
+				<form action="myentreprise.php" method="post">
+					<input type="number" name="idEntreprise" hidden value="<?php echo $entreprise->getId() ?>">
+					<input type="submit" name="Oui" class="btn btn-danger" value="Oui">
+					<input type="button" name="Non" class="btn btn-success" value="Non" onclick="Close()">
+				</form>
+			</div>
+		</div>
 
-    <script>
-        function Delete() {
-            document.getElementById('askDeleteBackground').style.display = 'block';
-        }
+		<!-- //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// -->
+	</div>
 
-        function Close() {
-            document.getElementById('askDeleteBackground').style.display = 'none';
-        }
-    </script>
+	<script>
+		function Delete() {
+			document.getElementById('cardBackground').style.display = 'block';
+			document.getElementById('deleteCard').style.display = 'block';
+		}
+
+		function Close() {
+			document.getElementById('cardBackground').style.display = 'none';
+			document.getElementById('deleteCard').style.display = 'none';
+		}
+	</script>
 
 </body>
 
--- pages/profil.php
+++ pages/profil.php
@@ -19,6 +19,7 @@ if (isset($_POST['BeASeller'])) {
 	if ($db->beASeller($user)) {
 		$_SESSION['seller'] = 2;
 	}
+	header('Location: profil.php');
 }
 if (isset($_POST['Disconnect'])) {
 	session_unset();
@@ -44,6 +45,13 @@ if (isset($_POST['adminUser'])) {
 if (isset($_POST['adminEntreprise'])) {
 	header('Location: adminEntreprise.php');
 }
+
+if (isset($_POST['Send'])) {
+	$to = "mgmehdi@tutanota.com";
+	$subject = "Utilisateur : ". $_SESSION['id']. " - ". utf8_decode($_POST['Subject']);
+	mail($to, $subject, utf8_decode($_POST['Message']));
+	header('Location: profil.php');
+}
 ?>
 
 <html lang="en">
@@ -60,7 +68,7 @@ if (isset($_POST['adminEntreprise'])) {
 	<div class="container-fluid">
 		<h1>Bienvenue <?php echo $_SESSION['surname'] ?></h1>
 		<form action="profil.php" method="post">
-			<?php if ($_SESSION['seller']==1 && !$_SESSION['admin']) { ?>
+			<?php if ($_SESSION['seller'] == 1 && !$_SESSION['admin']) { ?>
 				<input type="submit" value="Mes productions" name="MyProducts">
 			<?php } else if (!$_SESSION['seller'] && !$_SESSION['admin']) { ?>
 				<input type="button" value="Devenir un vendeur" onclick="Open('seller')">
@@ -73,55 +81,72 @@ if (isset($_POST['adminEntreprise'])) {
 			<input type="submit" value="Changer mes données" name="Update">
 			<input type="submit" value="Changer de mots de passe" name="Password">
 			<input type="submit" value="Déconnexion" name="Disconnect">
+			<input type="button" value="Demander un support" name="Contact" onclick="Open('support')">
 			<?php if (!$_SESSION['admin']) { ?>
 				<input type="button" value="Supprimer mon compte" onclick="Open('delete')">
 			<?php } ?>
 		</form>
 
-		<div id="askDeleteBackground" class="text-center" onclick="Close('delete')">
-			<div id="loginCard" class="card" style="width: 40rem;">
-				<div class="card-body">
-					<h3 class="card-title">Vous nous quittez ?</h3>
-					<h5>Etes-vous sûr de vouloir supprimer votre compte ?</h5>
-					<form action="profil.php" method="post">
-						<input type="submit" name="DeleteMe" class="btn btn-danger" value="Oui">
-						<input type="button" name="Non" class="btn btn-success" value="Non" onclick="Close('delete')">
-					</form>
-				</div>
+		<div id="cardBackground" class="text-center" onclick="Close('background')"></div>
+		<div id="deleteCard" class="card" style="width: 40rem;">
+			<div class="card-body">
+				<h3 class="card-title">Vous nous quittez ?</h3>
+				<h5>Etes-vous sûr de vouloir supprimer votre compte ?</h5>
+				<form action="profil.php" method="post">
+					<input type="submit" name="DeleteMe" class="btn btn-danger" value="Oui">
+					<input type="button" name="Non" class="btn btn-success" value="Non" onclick="Close()">
+				</form>
 			</div>
 		</div>
-		<div id="askBecomeSeller" class="text-center" onclick="Close('seller')">
-			<div id="loginCard" class="card" style="width: 40rem;">
-				<div class="card-body">
-					<h3 class="card-title">Devenir un vendeur ?</h3>
-					<h5>
-						Etes-vous sûr de vouloir devenir un vendeur ?<br>
-						Un modérateur devra valider cette demande.
-					</h5>
-					<form action="profil.php" method="post">
-						<input type="submit" name="BeASeller" class="btn btn-danger" value="Oui">
-						<input type="button" name="Non" class="btn btn-success" value="Non" onclick="Close('seller')">
-					</form>
-				</div>
+
+		<div id="sellerCard" class="card" style="width: 40rem;">
+			<div class="card-body">
+				<h3 class="card-title">Devenir un vendeur ?</h3>
+				<h5>
+					Etes-vous sûr de vouloir devenir un vendeur ?<br>
+					Un modérateur devra valider cette demande.
+				</h5>
+				<form action="profil.php" method="post">
+					<input type="submit" name="BeASeller" class="btn btn-danger" value="Oui">
+					<input type="button" name="Non" class="btn btn-success" value="Non" onclick="Close()">
+				</form>
+			</div>
+		</div>
+
+		<div id="helpCard" class="card" style="width: 40rem; top:-10em">
+			<div class="card-body">
+				<h3 class="card-title">Demander un support</h3>
+				<form action="profil.php" method="post">
+					<label for="Subject">Sujet</label>
+					<input type="text" class="form-control" name="Subject" id="">
+					<label for="Message">Votre message</label>
+					<textarea name="Message" id="Message" cols="" rows="10"></textarea>
+
+					<input type="submit" name="Send" class="btn btn-danger" value="Envoyer">
+					<input type="button" name="" class="btn btn-success" value="Anuler" onclick="Close()">
+				</form>
 			</div>
 		</div>
 	</div>
 
+
 	<script>
-		function Open(value) {			
+		function Open(value) {
+			document.getElementById('cardBackground').style.display = 'block';
 			if (value == 'delete') {
-				document.getElementById('askDeleteBackground').style.display = 'block';
+				document.getElementById('deleteCard').style.display = 'block';
 			} else if (value == 'seller') {
-				document.getElementById('askBecomeSeller').style.display = 'block';
+				document.getElementById('sellerCard').style.display = 'block';
+			} else if (value == 'support') {
+				document.getElementById('helpCard').style.display = 'block';
 			}
 		}
 
-		function Close(value) {
-			if (value == 'delete') {
-				document.getElementById('askDeleteBackground').style.display = 'none';
-			} else if (value == 'seller') {
-				document.getElementById('askBecomeSeller').style.display = 'none';
-			}
+		function Close() {
+			document.getElementById('cardBackground').style.display = 'none';
+			document.getElementById('deleteCard').style.display = 'none';
+			document.getElementById('sellerCard').style.display = 'none';
+			document.getElementById('helpCard').style.display = 'none';
 		}
 	</script>
 
